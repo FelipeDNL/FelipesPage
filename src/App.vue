d62dd2fe96243b13460b240e3cc9c326fcc13c0f
FelipeDNL <felipednlled@gmail.com>
2024-12-29 22:03:36 -0300

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Header from './components/Header.vue';
+import HalfBox from './components/HalfBox.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <Header />
+    <div class="content">
+      <HalfBox class="q1" style="flex: 1;">Quadro 1</HalfBox>
+      <HalfBox class="q2" style="flex: 3;">Quadro 2</HalfBox>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import HalfBox from './components/HalfBox.vue';
+
+export default {
+  components: {
+    Header,
+    HalfBox,
+  },
+};
+</script>
+
+<style>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  height: 95vh;
+  width: 95vw;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  gap: 10px; 
+}
+
+.content > * {
+  flex: 1;
+}
+</style>
---
+++ src/components/HalfBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="half-box">
+
+      <div class="local">
+        ../src/
+      </div>
+
+      <div class="opcoes">
+        ../src/
+      </div>
+      
+    </div>
+  </template>
+  
+  <style scoped>
+  .half-box {
+    flex: 1;
+    border: 2px solid #ccc;
+  }
+
+  .local{
+    background-color: dimgray;
+  }
+
+  .opcoes{
+    display: flex;
+    justify-content: flex-start;
+    padding-left: 5px;
+  }
+
+  .opcoes:hover{
+    background-color: #a2a1a1;
+  }
+  </style>
